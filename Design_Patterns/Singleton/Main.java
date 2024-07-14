f8a5951955ea8d8f846df8e37ca0324ebf0ce441
I563718 <nalin.sharma@sap.com>
2024-07-14 21:34:35 +0530

    Thread safe singleton

--- Design_Patterns/Singleton/Main.java
+++ Design_Patterns/Singleton/Main.java
@@ -1,11 +1,38 @@
 package Design_Patterns.Singleton;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
 
-        System.out.println(obj1);
-        System.out.println(obj2);
+//        Singleton obj1 = Singleton.getInstance();
+//        System.out.println(obj1);
+//        Singleton obj2 = Singleton.getInstance();
+//        System.out.println(obj2);
+
+//        ********** Thread Safe Singleton **********
+
+//        new Thread(() -> {
+//            final Singleton obj1 = Singleton.getInstance();
+//            System.out.println(obj1);
+//        }, "Thread1").start();
+//
+//        new Thread(() -> {
+//            final Singleton obj2 = Singleton.getInstance();
+//            System.out.println(obj2);
+//        }, "Thread2").start();
+
+        //we need to use executor service to call the singleton class at the same time by diff threads
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(() -> {Singleton obj1 = Singleton.getInstance();
+            System.out.println(obj1);
+        });
+        executor.execute(() -> {Singleton obj1 = Singleton.getInstance();
+            System.out.println(obj1);
+        });
+
+
     }
 }
--- Design_Patterns/Singleton/Singleton.java
+++ Design_Patterns/Singleton/Singleton.java
@@ -2,18 +2,24 @@ package Design_Patterns.Singleton;
 
 public class Singleton {
 
+    //    ****** Thread Safe Singleton ******
+
     //obj is static because we will always have one objects per class
-    static Singleton obj = null;
+    //using volatile so that the local memory of each thread is matched with global value in real time which is shared by each thread
+    //otherwise delay in global memory update can cause race condition, volatile keyword will make sure that the value of obj is same in all threads
+    private static volatile Singleton obj = null;
 
     //making constuctor private so that no one can create object of this class
     private Singleton() {};
 
     //this method will return the object of this class
     public static Singleton getInstance() {
-        if(obj == null) {
-            obj = new Singleton();
+        if(obj == null) {   //optimization
+            synchronized (Singleton.class) {
+                if(obj == null)     //double checking
+                    obj = new Singleton();
+            }
         }
         return obj;
     }
-
 }
