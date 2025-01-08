226c6514e4949b7b95a1fdc4609d9e43f8d2d985
nalinsharma9 <nalin.sharma1998@gmail.com>
2025-01-08 16:51:18 +0530

    fix

--- MultiThreading/CustomMutex.java
+++ MultiThreading/CustomMutex.java
@@ -15,7 +15,7 @@ public class CustomMutex {
     private boolean isLocked = false;
 
     public synchronized void acquire() throws InterruptedException {
-//        if (isLokced) {
+//        if (isLocked) {
         while (isLocked) {          //while is critical to check condition again after the thread is awakened,
             this.wait();            // because maybe multiple threads were waiting to be awakened, and they did not check the condition again
         }
@@ -24,6 +24,6 @@ public class CustomMutex {
 
     public synchronized void release() {
         isLocked = false;
-        this.notify();
+        this.notifyAll();
     }
 }
