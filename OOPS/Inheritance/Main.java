25900e6daa81a11864d299de7fabc8f2e4042bc7
nalinsharma9 <nalin.sharma1998@gmail.com>
2025-05-14 00:59:17 +0530

    Inheritance basics

--- OOPS/Inheritance/Main.java
+++ OOPS/Inheritance/Main.java
@@ -1,5 +1,9 @@
 package OOPS.Inheritance;
 
+/*
+All the public and protected methods are inherited by Child class from Parent class.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,8 +12,16 @@ public class Main {
 
 //        Default constructors of parent class are implicitly called by all child class constructor in Java
 //        However, parametarized constructors of super class needs to be handled by explicit call
-        BoxWeight bw1 = new BoxWeight();
-        System.out.println(bw1);
+//        BoxWeight bw1 = new BoxWeight();
+//        System.out.println(bw1);
+
+//        b3 tries to find overriden run() method in BoxWeight class but since it's not their it calls run() of parent class
+
+        BoxWeight bw3 = new BoxWeight();
+        bw3.run();
+
+        Box b3 = new BoxWeight();
+        b3.run();
 
 //        l is a private member, so it cannot be accessed from outside the class
 //        System.out.println(bw1.l);
@@ -22,10 +34,6 @@ public class Main {
 
 //        System.out.println(b2.weight);    //Access and the method signature is decided by the type of reference variable hence we can access only the members of Box class
 //        System.out.println(b2.run);       //Child class object searches for parent method if the method is not found in child
-
-        //b3 tries to find run() method in BoxWeight class but since it's not their it calls run() of parent class.
-//        Box b3 = new BoxWeight();
-//        b3.run();
 
         //BoxWeight has access to the weight but box class cannot initialize weight property. No keyword to call child class constructor
         // hence the error.
